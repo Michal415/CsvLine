cad7e7c1e208ce990446405ea84ff03247a90e35
Michall <winiarskimichal1@wp.pl>
2019-10-02 21:15:34 +0200

    csv v.3 row

--- Hello.py
+++ Hello.py
@@ -6,5 +6,7 @@ with open("f:\\pliki\\zeszyt1.csv") as plik:
         if odczyt.line_num == 1:
             continue
         lista.append(i)
+with open("f:\\pliki\\zapis.csv","w", newline='') as zapis2:
     zapis  =csv.writer(zapis2)
-    zapis.writerows(lista)
+    for j in lista:
+        zapis.writerow(j)
