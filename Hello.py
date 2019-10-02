f313c4a78a05a50eb2bac9fcf95dc6d9bc4eb8c7
Michall <winiarskimichal1@wp.pl>
2019-10-02 20:46:31 +0200

    csv v.1

---
+++ Hello.py
@@ -0,0 +1,11 @@
+import csv
+lista = []
+with open("f:\\pliki\\zeszyt1.csv") as plik:
+    odczyt =csv.reader(plik)
+    for i in odczyt:
+        if odczyt.line_num == 1:
+            continue
+        lista.append(i)
+with open("f:\\pliki\\zapis.csv","w", newline='') as zapis2:
+    zapis  =csv.writer(zapis2)
+    zapis.writerows(lista)
